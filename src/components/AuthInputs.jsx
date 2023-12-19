0ecbbdfe132b89f5d00d27d50437f9c6c0b10050
TNTatOffice <tmoscovich@vidazoo.com>
2023-12-19 14:58:50 +0200

    V2
    Added CSS Module to Header

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -24,18 +24,29 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          {/* Setting className on label dynamically so taht different CSS rules can apply as needed */}
+          {/* Setting it to a template literal with `` used to create Strings*/}
+          {/* The className will always have label and in addition to it- invalid or nothing, depending on the candition */}
+          <label className={`label${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
+            // Dynamically changing color of background for email based on condition
+            // style={{
+            //   backgroundColor: emailNotValid ? '#ef4444' : '#d1d5db',
+            // }}
+            // Use a class name to style conditionally and not &&. If you use &&, the className will become emailNotValid
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label${passwordNotValid ? 'invalid' : ''}`}>Password</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
+            // style={{
+            //   background: passwordNotValid ? '#ef4444' : '#d1d5db',
+            // }}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import logo from '../assets/logo.png';
+// importing CSS module for styling.
+import classes from './Header.module.css'
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      {/* Using a css module on P */}
+      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
