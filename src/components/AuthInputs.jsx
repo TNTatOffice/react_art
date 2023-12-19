6bbdbfc2136dd482889d736779015c03ff0ef6f9
TNTatOffice <tmoscovich@vidazoo.com>
2023-12-19 16:55:28 +0200

    Import Styled Components
    Added Styled Components and applied conditionally on labels and user input

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -11,7 +11,27 @@ flex-direction: column;
 gap: 0.5rem;
 margin-bottom: 1.5rem;
 `
-
+const Label = styled.label`
+display: block;
+margin-bottom: 0.5rem;
+font-size: 0.75rem;
+font-weight: 700;
+letter-spacing: 0.1em;
+text-transform: uppercase;
+// Passing conditional style as a function to set styling dynamically below
+color: ${({ invalid }) => invalid ? '#f87171' : '#6b7280'};
+`
+const Input = styled.input`
+width: 100%;
+padding: 0.75rem 1rem;
+line-height: 1.5;
+background-color: ${({ invalid }) => invalid ? '#fed2d2' : '#d1d5db'} ;
+color: #374151;
+border: 1px solid  ${({ invalid }) => invalid ? '#f73f3f' : 'transparent'};
+border-radius: 0.25rem;
+box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+color: ${({ invalid }) => invalid ? '#ef4444' : '#6b7280'};
+`
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -40,23 +60,23 @@ export default function AuthInputs() {
           {/* Setting className on label dynamically so taht different CSS rules can apply as needed */}
           {/* Setting it to a template literal with `` used to create Strings*/}
           {/* The className will always have label and in addition to it- invalid or nothing, depending on the candition */}
-          <label className={`label${emailNotValid ? 'invalid' : ''}`}>Email</label>
-          <input
+          <Label invalid={emailNotValid}>
+            Email
+          </Label>
+          <Input
+            invalid={emailNotValid}
             type="email"
-            // Dynamically changing color of background for email based on condition
-            // style={{
-            //   backgroundColor: emailNotValid ? '#ef4444' : '#d1d5db',
-            // }}
-            // Use a class name to style conditionally and not &&. If you use &&, the className will become emailNotValid
-            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label className={`label${passwordNotValid ? 'invalid' : ''}`}>Password</label>
-          <input
+          {/* Dynamic styling applied with invalid on Password label */}
+          <Label invalid={passwordNotValid}>
+            Password
+          </Label>
+          <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            invalid={passwordNotValid}
             // style={{
             //   background: passwordNotValid ? '#ef4444' : '#d1d5db',
             // }}
